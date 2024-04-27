7da34f19b67ca6c6f864172e450dba0005db0cfc
dominikaglow <dominika01.glow@gmail.com>
2024-04-27 17:23:10 +0200

    Created model for users and DataContext

---
+++ WebAPI/Model/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Model
+{
+    public class User
+    {
+        public long Id { get; set; }
+        [Required]
+        [MinLength(5, ErrorMessage ="Username must be at least 5 characters long")]
+        public string Username { get; set; }
+        [Required]
+        [MinLength(7, ErrorMessage = "Password must be at least 7 characters long")]
+        public int Password { get; set; }
+        [Required]
+        public string CompanyName { get; set; }
+        [Required]
+        public string Email { get; set; }
+    }
+}
